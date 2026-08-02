dba70842329daad25cfb3a02b50c4cdf231b690c
chisus-dev <denis.chisus@gmail.com>
2026-08-03 01:39:41 +0300

    Harden LexiCards Android WebView shell

--- vocab-cards/app/src/main/java/dev/chisus/lexicards/MainActivity.java
+++ vocab-cards/app/src/main/java/dev/chisus/lexicards/MainActivity.java
@@ -2,38 +2,68 @@ package dev.chisus.lexicards;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+    private static final int APP_BACKGROUND = Color.rgb(13, 16, 21);
     private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(APP_BACKGROUND);
+        getWindow().setNavigationBarColor(APP_BACKGROUND);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
         webView = new WebView(this);
+        webView.setBackgroundColor(APP_BACKGROUND);
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(false);
-        settings.setDatabaseEnabled(true);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
+        settings.setTextZoom(100);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
         webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
 
     @Override
     public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
+        if (webView == null) {
             super.onBackPressed();
+            return;
         }
+        webView.evaluateJavascript(
+            "window.lexiCardsHandleBack ? window.lexiCardsHandleBack() : false",
+            handled -> {
+                if (!"true".equals(handled)) MainActivity.super.onBackPressed();
+            }
+        );
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
     }
 }
